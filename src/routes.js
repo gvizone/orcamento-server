d4bf957f18a412b523d7f5fa835cdb7aa6fb9ece
Gabriel Vizone <gabriel.vizone@gmail.com>
2019-02-17 17:08:50 -0300

    Add rotas

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,17 @@ const express = require('express');
 
 const routes = express.Router();
 
-const ObjetoController = require('./controllers/ObjetoController');
+const UserController = require('./controllers/UserController');
+routes.get('/user', UserController.list);
+routes.post('/user', UserController.store);
+routes.post('/user/login', UserController.login);
 
-routes.get('/api', ObjetoController.index);
-routes.post('/api', ObjetoController.store);
+const AccountController = require('./controllers/AccountController');
+routes.get('/account/:userId', AccountController.accountsByUserId);
+routes.post('/account', AccountController.store);
+
+const TransactionController = require('./controllers/TransactionController');
+routes.get('/transaction/:accountId', TransactionController.listByAccountId);
+routes.post('/transaction', TransactionController.store);
 
 module.exports = routes;
