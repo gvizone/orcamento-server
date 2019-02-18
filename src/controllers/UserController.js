a39d25b511d21d4984f4178007064c60eb27d4af
Gabriel Vizone <gabriel.vizone@gmail.com>
2019-02-18 20:58:51 -0300

    Refatoração das queries

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,19 +1,32 @@
 const User = require('../models/User');
+const utils = require('../utils/utils');
 
 module.exports = {
   async list (req, res) {
     console.log(req);
-    const users = await User.find({}).sort('-createdAt');
-    return res.json(users);
+    const users = await User
+      .find({})
+      .sort('-createdAt')
+      .exec((err, model) => {
+        if (err) return res.json(utils.handleError(err));
+        return res.json(utils.handleSuccess(model));
+      });
   },
   async login (req, res) {
-    console.log(req);
-    const user = await User.findOne({ email: req.body.email, password: req.body.password });
-    return res.json(user);
+    const access = { email: req.body.email, password: req.body.password };
+    const user = await User
+      .findOne(access, (err, data) => {
+        if (err) return res.json(utils.handleError(err));
+        if (data) return res.json(utils.handleSuccess(data));
+        res.status(404);
+        return res.json(utils.handleError('user or password invalid'));
+      });
   },
   async store (req, res) {
-    const user = await User.create(req.body);
-    req.io.emit('user', user);
-    return res.json(user);
+    const user = await User.create(req.body, (err, data) => {
+      if (err) return res.json(utils.handleError(err));
+      req.io.emit('user', data);
+      return res.json(utils.handleSuccess(data));
+    });
   }
 };
