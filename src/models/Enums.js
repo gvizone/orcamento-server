ff7b4d113cdf0cbef734c94f0536f8aca279d446
Gabriel Vizone <gabriel.vizone@gmail.com>
2019-02-18 22:39:35 -0300

    Add arquivo de enums

---
+++ src/models/Enums.js
@@ -0,0 +1,18 @@
+module.exports = {
+  AccountType: ['Cartão de crédito', 'Fundo de Investimento', 'Conta Corrente', 'Poupança'],
+  TransactionType: ['ENTRADA', 'SAIDA'],
+  Category: [
+    'Gastos Essenciais',
+    'Estilo De Vida',
+    'Lançamento Entre Contas',
+    'Renda',
+    'Não Classificado'
+  ],
+  SubCategory: [
+    'Contas Residenciais', 'Educação', 'Mercado', 'Moradia', 'Saúde', 'Transporte',
+    'Bares/Restaurantes', 'Compras', 'Cuidados Pessoais', 'Impostos', 'Lazer',
+    'Presentes', 'Serviços', 'TV/Internet/Telefone', 'Taxas Bancárias', 'Viagem',
+    'Aplicação', 'TED', 'Pagamento de Cartão', 'Remuneração', 'Rendimento',
+    'Bônus', 'Outras Rendas', 'Categorizar'
+  ]
+};
