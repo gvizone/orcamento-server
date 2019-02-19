19afd4e93183fab8e8231a616c24e3fc358ba3a7
Gabriel Vizone <gabriel.vizone@gmail.com>
2019-02-18 21:41:32 -0300

    Verificação se conta existe

--- src/controllers/TransactionController.js
+++ src/controllers/TransactionController.js
@@ -11,8 +11,17 @@ module.exports = {
       });
   },
   async store (req, res) {
-    const transaction = await Transaction.create(req.body);
-    req.io.emit('transaction', transaction);
-    return res.json(transaction);
+    await Account.findOne({ _id: req.body.account }, (err, data) => {
+      if (err) return res.json(utils.handleError(err));
+      if (!data) {
+        res.status(404);
+        return res.json(utils.handleError('Account not found'));
+      }
+      Transaction.create(req.body, (err, data) => {
+        if (err) return res.json(utils.handleError(err));
+        req.io.emit('transaction', data);
+        return res.json(utils.handleSuccess(data));
+      });
+    });
   }
 };
