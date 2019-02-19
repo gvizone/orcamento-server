d40787a796fb139c4d946f9a9e306d59f2c16384
Gabriel Vizone <gabriel.vizone@gmail.com>
2019-02-18 22:40:27 -0300

    Add categoria e subcategoria na transação

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
+const Enum = require('./Enums');
 
 const TransactionSchema = new mongoose.Schema({
   name: String,
   value: Number,
   date: Date,
   description: String,
-  type: { type: String, enum: ['ENTRADA', 'SAIDA'], required: true },
+  type: { type: String, enum: Enum.TransactionType, required: true },
+  category: { type: String, enum: Enum.Category, default: 'Não Classificado' },
+  subCategory: { type: String, enum: Enum.SubCategory, default: 'Categorizar' },
   account: { type: mongoose.Schema.Types.ObjectId, ref: 'Account', required: true },
   createdAt: { type: Date, default: Date.now }
 });
